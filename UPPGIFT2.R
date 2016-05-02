c7d3fe99961a80a832a060428c6d6cc2420af2ef
afuyo <artmro@gmail.com>
2016-05-02 23:02:14 +0200

    first commit

---
+++ UPPGIFT2.R
@@ -0,0 +1,9 @@
+#uppgift2
+#med 3% så förkastar vi nullhop om  xbar är 216.825
+#det är gränvärdet
+xbarc<-218+(-(1.88*5)/8)
+#type II error
+#
+(216.825-215)/(5/sqrt(64))
+#probability of typeII error 0.001750157
+pnorm(2.92,lower.tail = FALSE)
